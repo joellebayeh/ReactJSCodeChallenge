83aaae1ab8c3861cd9d05f164ea82704b7bd224d
unknown <joellebayeh55@gmail.com>
2022-12-01 11:14:52 +0200

    add routing

---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,5 @@
+const Dashboard = () => {
+    return <h1>this is the Dashboard page !</h1>
+}
+
+export default Dashboard;
---
+++ src/pages/Login/LoginForm.js
@@ -0,0 +1,5 @@
+const LoginForm = () => {
+    return <h1>this is the login page !</h1>
+}
+
+export default LoginForm;
